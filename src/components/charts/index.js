585bb418e18bc0c71a67073744658f139ff9dd18
John Osterman <josterman@pewtrusts.org>
2020-08-05 11:15:25 -0400

    Colors

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -209,7 +209,7 @@ export function initCharts({filters = [], sortBy = 'ev', sortDirection = 'desc',
                 .attr('data-key', d => d.key);
             
                 entering.append('h2')
-                    .html(d => `<span>${filters.reduce((acc, cur, i) => acc + '&middot; ','')}${display(d.key)}</span> <span class="${s.filtersDisplay}">${displayFilters(filters)}</span>`);
+                    .html(d => `<span>${display(d.key)}</span> <span class="${s.filtersDisplay}">${displayFilters(filters)}</span>`);
 
             if ( filters.length === 0 ){ //only add graf when no filters applied, ie, main tables
 
